c400cdabb0faf7f14c992d6f0c04e8196d57860e
H. Watanabe <kaityo@users.sourceforge.jp>
2019-03-09 21:20:50 +0900

    dig

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-#include "sudoku_lib/grid.hpp"
 #include "sudoku_lib/ansmaker.hpp"
+#include "sudoku_lib/grid.hpp"
+#include <iostream>
 
-int main(){
+void dig(const std::string &answer) {
+  mbit m = mbit81mask;
+  int hints = 81;
+  for (int i = 0; i < 1000; i++) {
+    mbit m2 = Mbit::random_remove(m, mt);
+    std::string s = mbit2str(m2, answer);
+    if (is_unique(m2)) {
+      hints--;
+      m = m2;
+      if (hints == 30) {
+        ts.look_around(m, answer, mout);
+        return;
+      }
+    }
+  }
+}
+
+int main() {
   AnsMaker am;
-  std::cout << am.make() << std::endl;
+  dig(am.make());
 }
