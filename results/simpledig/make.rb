f11f0de21a3f3ca01ceb6e008cc14cd2d0e64d68
H. Watanabe <kaityo@users.sourceforge.jp>
2019-03-13 16:14:03 +0900

    adds results

--- results/simpledig/make.rb
+++ results/simpledig/make.rb
@@ -1,20 +1,47 @@
 
-out = File.open("q2.dat","w")
+out = File.open("q2.dat", "w")
+arr = []
 `ls q2*.dat`.split(/\n/).each do |file|
   File.open(file) do |f|
     while line = f.gets
       a = line.split(/\s/)
-      out.puts "#{a[2]} #{a[1]}"
+      arr.push [a[2].to_i, a[1].to_i]
     end
   end
 end
 
-out = File.open("sc.dat","w")
+arr.sort! do |a, b|
+  if a[0] == b[0]
+    a[1] <=> b[1]
+  else
+    a[0] <=> b[0]
+  end
+end
+
+arr.each do |v|
+  out.puts "#{v[0]} #{v[1]}"
+end
+
+arr = []
+
+out = File.open("sc.dat", "w")
 `ls sc*.dat`.split(/\n/).each do |file|
   File.open(file) do |f|
     while line = f.gets
       a = line.split(/\s/)
-      out.puts "#{a[2]} #{a[1]}"
+      arr.push [a[2].to_i, a[1].to_i]
     end
   end
 end
+arr.sort! do |a, b|
+  if a[0] == b[0]
+    a[1] <=> b[1]
+  else
+    a[0] <=> b[0]
+  end
+end
+
+arr.each do |v|
+  out.puts "#{v[0]} #{v[1]}"
+end
+
