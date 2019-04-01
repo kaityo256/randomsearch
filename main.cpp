978ab7fe28102af20170eaeab680e4fe7a6dce2c
H. Watanabe <kaityo@users.sourceforge.jp>
2019-04-01 15:35:01 +0900

    モードをオプションで指定できるようにした

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "cmdline.h"
 #include "sc125f/sc125fa.hpp"
 #include "smr/rater.hpp"
 #include "smr/uafinder.hpp"
@@ -19,8 +20,11 @@ UniquenessChecker uc;
 std::ofstream ofs_q2;
 std::ofstream ofs_sc;
 
-// このマクロが有効な場合、途中から全探索に切り替える
-//#define FULLSEARCH
+//グローバルフラグ
+bool calc_sc125f = true;     // sc125fを計算する
+bool calc_gsfq2 = true;      // gsf's q2を計算する
+bool use_fullsearch = false; //途中から全探索に切り替える
+bool use_uasets = false;     // 解の一意性判定に不可避集合を用いる
 
 // プログラム開始時からの経過時間(ms)
 long long myclock() {
@@ -34,19 +38,23 @@ long long myclock() {
 void show(std::string &str) {
   static long long q2max = 0;
   static int scmax = 0;
-  auto r = rater.rate(str.c_str());
-  if (q2max < r) {
-    q2max = r;
-    ofs_q2 << str << " ";
-    ofs_q2 << r << " ";
-    ofs_q2 << myclock() << std::endl;
+  if (calc_gsfq2) {
+    auto r = rater.rate(str.c_str());
+    if (q2max < r) {
+      q2max = r;
+      ofs_q2 << str << " ";
+      ofs_q2 << r << " ";
+      ofs_q2 << myclock() << std::endl;
+    }
   }
-  int sc = sc125f::calc(str.c_str());
-  if (scmax < sc) {
-    scmax = sc;
-    ofs_sc << str << " ";
-    ofs_sc << sc << " ";
-    ofs_sc << myclock() << std::endl;
+  if (calc_sc125f) {
+    int sc = sc125f::calc(str.c_str());
+    if (scmax < sc) {
+      scmax = sc;
+      ofs_sc << str << " ";
+      ofs_sc << sc << " ";
+      ofs_sc << myclock() << std::endl;
+    }
   }
 }
 
@@ -58,48 +66,36 @@ bool hits_uasets(const mbit &m) {
   return true;
 }
 
-bool is_unique(const mbit &m) {
-  if (!hits_uasets(m)) return false;
-  uint64_t *n = (uint64_t *)(&m);
-  const Mask128 mm(n[0], n[1]);
-  return uc.isSolutionUnique(mm);
+bool is_unique(const mbit &m, const std::string &answer) {
+  // 不可避集合とantaさんの一意性判定ルーチンを利用
+  if (use_uasets) {
+    if (!hits_uasets(m)) return false;
+    uint64_t *n = (uint64_t *)(&m);
+    const Mask128 mm(n[0], n[1]);
+    return uc.isSolutionUnique(mm);
+  } else {
+    // 自前の一意性判定ルーチンを利用
+    std::string str = mbit2str(m, answer);
+    return Grid::is_unique(str);
+  }
 }
 
 // 途中までランダムに掘った数独から、掘れるだけ掘って探す
-void search(mbit s1, mbit s2, const std::string &answer) {
+void fullsearch(mbit s1, mbit s2, const std::string &answer) {
   if (s2 == 0) {
+    if (!is_unique(s1, answer)) return;
     std::string str = mbit2str(s1, answer);
-    if (!Grid::is_unique(str)) return;
     show(str);
-    //std::cout << str << std::endl;
     return;
   }
   mbit lsb = (s2 & -s2);
   s2 ^= lsb;
-  search(s1, s2, answer);
+  fullsearch(s1, s2, answer);
   mbit s3 = s1 ^ lsb;
-  std::string str2 = mbit2str(s3, answer);
-  if (!Grid::is_unique(str2)) return;
-  search(s3, s2, answer);
+  if (!is_unique(s3, answer)) return;
+  fullsearch(s3, s2, answer);
 }
 
-//途中までランダムに掘った数独から、掘れるだけ掘って探す(不可避集合利用版)
-// TODO: 上とまとめること
-void search_withuc(mbit s1, mbit s2, const std::string &answer) {
-  if (s2 == 0) {
-    if (!is_unique(s1)) return;
-    std::string str = mbit2str(s1, answer);
-    show(str);
-    return;
-  }
-  mbit lsb = (s2 & -s2);
-  s2 ^= lsb;
-  search(s1, s2, answer);
-  mbit s3 = s1 ^ lsb;
-  std::string str2 = mbit2str(s3, answer);
-  if (!is_unique(s3)) return;
-  search(s3, s2, answer);
-}
 mbit random_remove(const mbit &s, std::mt19937 &mt) {
   mbit t = s;
   std::uniform_int_distribution<> ud(0, popcnt_u128(s) - 1);
@@ -115,55 +111,21 @@ void dig(const std::string &answer, std::mt19937 &mt) {
   int hints = 81;
   for (int i = 0; i < 100; i++) {
     mbit m2 = random_remove(m, mt);
-    std::string s = mbit2str(m2, answer);
-    if (!Grid::is_unique(s)) {
-#ifndef FULLSEARCH
-      std::string str = mbit2str(m, answer);
-      show(str);
-      //std::cout << str << std::endl;
-#endif
+    if (!is_unique(m2, answer)) {
+      if (!use_fullsearch) {
+        std::string str = mbit2str(m, answer);
+        show(str);
+      }
       return;
     }
     hints--;
     m = m2;
-#ifdef FULLSEARCH
-    if (hints == 30) {
-      search(m, m, answer);
-      return;
+    if (use_fullsearch) {
+      if (hints == 30) {
+        fullsearch(m, m, answer);
+        return;
+      }
     }
-#endif
-  }
-}
-
-void test(const int seed = 0) {
-  std::mt19937 mt;
-  AnsMaker am(seed);
-  mt.seed(seed);
-  char filename[256];
-  sprintf(filename, "sc_%04d.dat", seed);
-  ofs_sc.open(filename);
-  sprintf(filename, "q2_%04d.dat", seed);
-  ofs_q2.open(filename);
-  auto ans = am.make();
-  while (true) {
-    dig(ans, mt);
-  }
-}
-
-void use_uasets(const int seed = 0) {
-  std::mt19937 mt;
-  AnsMaker am(seed);
-  mt.seed(seed);
-  char filename[256];
-  sprintf(filename, "sc_%04d.dat", seed);
-  ofs_sc.open(filename);
-  sprintf(filename, "q2_%04d.dat", seed);
-  ofs_q2.open(filename);
-  auto ans = am.make();
-  uasets.clear();
-  uafinder.findAll(ans.c_str(), 4);
-  while (true) {
-    dig(ans, mt);
   }
 }
 
@@ -176,57 +138,64 @@ mbit mask81tombit(const Mask81 &mask) {
   return m;
 }
 
-void dig_withuc(const std::string &answer, std::mt19937 &mt) {
-  mbit m = mask81;
-  int hints = 81;
-  for (int i = 0; i < 100; i++) {
-    mbit m2 = random_remove(m, mt);
-    std::string str = mbit2str(m2, answer);
-    if (!is_unique(m2)) {
-      return;
-    }
-    hints--;
-    m = m2;
-    if (hints == 30) {
-      search_withuc(m, m, answer);
-      return;
-    }
-  }
-}
-
-void uatest(const int seed = 0) {
-  std::mt19937 mt;
-  AnsMaker am(seed);
-  mt.seed(seed);
-  char filename[256];
-  sprintf(filename, "sc_%04d.dat", seed);
-  ofs_sc.open(filename);
-  sprintf(filename, "q2_%04d.dat", seed);
-  ofs_q2.open(filename);
-
-  auto ans = am.make();
-  uc.setSolution(ans.c_str());
-  // 不可避集合のリストアップ
+// 不可避集合のビットパターンリストアップ
+void setup_uasets(const std::string &answer) {
+  uc.setSolution(answer.c_str());
   int max_uasize = 8;
-  uafinder.findAll(ans.c_str(), max_uasize);
+  uafinder.findAll(answer.c_str(), max_uasize);
   for (int i = 0; i <= max_uasize; i++) {
     auto ualist = uafinder.getList(i);
     for (const auto &mask : uafinder.getList(i)) {
       uasets.push_back(mask81tombit(mask));
     }
   }
+}
+
+void test(const int seed = 0) {
+  std::mt19937 mt;
+  AnsMaker am(seed);
+  mt.seed(seed);
+  auto ans = am.make();
+  if (use_uasets) {
+    setup_uasets(ans);
+  }
   while (true) {
-    dig_withuc(ans, mt);
+    dig(ans, mt);
   }
 }
 
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
+  cmdline::parser a;
+  a.add<std::string>("sc125f", 's', "Calculate sc125f", false, "yes", cmdline::oneof<std::string>("yes", "no"));
+  a.add<std::string>("gsfq2", 'q', "Calculate gsf's q2", false, "yes", cmdline::oneof<std::string>("yes", "no"));
+  a.add<std::string>("fullsearch", 'f', "Use Fullsearch", false, "no", cmdline::oneof<std::string>("yes", "no"));
+  a.add<std::string>("uasets", 'u', "Use UA Sets", false, "no", cmdline::oneof<std::string>("yes", "no"));
+  a.parse_check(argc, argv);
+  calc_sc125f = a.get<std::string>("sc125f") == "yes";
+  calc_gsfq2 = a.get<std::string>("gsfq2") == "yes";
+  use_fullsearch = a.get<std::string>("fullsearch") == "yes";
+  use_uasets = a.get<std::string>("uasets") == "yes";
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   if (rank == 0) {
-    std::cout << "Use UA sets" << std::endl;
+    std::cout << "Calculate gsf's q2: " << a.get<std::string>("gsfq2") << std::endl;
+    std::cout << "Calculate sc125f  : " << a.get<std::string>("sc125f") << std::endl;
+    std::cout << "Use Fullsearch    : " << a.get<std::string>("fullsearch") << std::endl;
+    std::cout << "Use UA Sets       : " << a.get<std::string>("uasets") << std::endl;
   }
-  uatest(rank);
+  // File Open
+  if (calc_sc125f) {
+    char filename[256];
+    sprintf(filename, "sc_%04d.dat", rank);
+    ofs_sc.open(filename);
+  }
+  if (calc_gsfq2) {
+    char filename[256];
+    sprintf(filename, "q2_%04d.dat", rank);
+    ofs_q2.open(filename);
+  }
+
+  test(rank);
   MPI_Finalize();
 }
